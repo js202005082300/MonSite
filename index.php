bbe8e2ae4f76af6ca6cb2f14ef15341806059401
js <js201910271836@outlook.com>
2021-11-27 01:31:29 +0100

    27-11-21 1:31:28

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
-echo "Ceci est une page test de déploiement via GitHub ...";
+echo "Ceci est une page test de déploiement via GitHub ...<br>";
 echo "Ceci est une phrase test !";
+
+
+
+echo '<br>';
+echo '<br>';
+echo '<br>';
+echo 'Nouvelle ligne';
 ?>
