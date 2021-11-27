8dfb7be590047b92895de926497684db290c9933
js <js201910271836@outlook.com>
2021-11-27 02:06:40 +0100

    27-11-21 2:06:39

---
+++ Database.php
@@ -0,0 +1,82 @@
+<?php
+
+class Database
+{
+    private $_connection;
+    private static $_instance = null;
+    
+    private static $DB_DSN = 'mysql:host=localhost;dbname=u870391923_MyDB';
+    private static $DB_USER = 'u870391923_root';
+    private static $DB_PASS = 'o8jtuHhZPmLXiVUZoj';
+
+    private function __construct()
+    {
+        try
+        {
+            $options =
+            [
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_EMULATE_PREPARES => false
+            ];
+
+            $this->_connection = new PDO(self::$DB_DSN, self::$DB_USER, self::$DB_PASS, $options);
+        }
+        catch(PDOException $e)
+        {
+            die("ERREUR : " . $e->getMessage());
+        }
+    }
+
+    public static function getInstance()
+    {
+        if(is_null(self::$_instance))
+            self::$_instance = new Database();
+
+        return self::$_instance;
+    }
+
+    public function getConnection()
+    {
+        return $this->_connection;
+    }
+
+    public function freeConnection()
+    {
+        return $this->_connection = NULL;
+    }
+
+    public function request($sql, $fields)
+    {
+        try
+        {
+            $req = $this->getConnection()->prepare($sql);
+            $req->execute($fields);
+
+            // if($req)
+            //     echo "<script type= 'text/javascript'>alert('Requête exécutée avec succès.');</script>";
+
+            return $req->fetch(PDO::FETCH_ASSOC);
+        }
+        catch(PDOException $e)
+        {
+            // echo "<script type= 'text/javascript'>alert('Requête non exécutée.');</script>";
+
+            die("ERREUR : " . $e->getMessage());
+        }
+    }
+
+    public function display()
+    {
+        $sql = 'SELECT * FROM table_dht ORDER BY id_dht DESC LIMIT 5;';
+
+        try
+        {
+            return $req = $this->getConnection()->query($sql)->fetchall(PDO::FETCH_ASSOC);
+        }
+        catch(PDOException $pe)
+        {
+            echo 'ERREUR : '.$pe->getMessage();
+        }
+    }
+}
--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
+require 'Database.php';
+?>
 
+<?php
 $dsn = 'mysql:host=localhost;dbname=u870391923_MyDB';
 $username = 'u870391923_root';
 $password = 'o8jtuHhZPmLXiVUZoj';
@@ -26,3 +29,61 @@ if(isset($_POST['success']) && !empty($_POST['success']))
 $conn=null;echo "Connexion MySQL fermée !\r\n";
 echo "------------------- Fin programme ! ------------------- \r\n\r\n";
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Samuel Jacquet</title>
+</head>
+<body class="container">
+	<header class="item">
+		<h1>Tableau de bord</h1>
+	</header>
+
+    <main class="item">
+        <section>
+            <h1>Page de collecte de données en local</h1>
+            <div>
+                <h2>Données DHT</h2>
+                <table cellspacing="5" cellpadding="5">
+                    <tr> 
+                        <td>DATE</td> 
+                        <td>HUMIDITY</td> 
+                        <td>TEMPERATURE</td> 
+                        <td>SUCCESS</td> 
+                    </tr>
+                    <?php 
+                        $dht_sql = Database::getInstance()->display();
+                        foreach($dht_sql as $row):
+                            $row_id = $row['id_dht'];
+                            $row_date = $row['dht_date'];
+                            $row_humidity = $row['dht_humidity'];
+                            $row_temperature = $row['dht_temperature'];
+                            $row_success = $row['dht_success'];
+                            $row_date = date("Y-m-d H:i:s", strtotime("$row_date"));
+                            echo '<tr>
+                            <td>'.$row_date.'</td>
+                            <td>'.$row_humidity.'</td>
+                            <td>'.$row_temperature.'</td>
+                            <td>'.$row_success.'</td>
+                            </tr>';
+                        endforeach;
+                    ?>
+                </table>
+            </div>
+        </section>
+    </main>
+
+    <aside class="item">
+    </aside>
+
+    <footer class="item">
+        <p>Copyright &copy; 2021</p>
+    </footer>
+
+</body>
+</html>
