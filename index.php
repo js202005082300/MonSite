f2e8e67d1e9d393ebc118a7b09c53d78fd782f1a
js <js201910271836@outlook.com>
2021-11-27 02:08:56 +0100

    27-11-21 2:08:55

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $username = 'u870391923_root';
 $password = 'o8jtuHhZPmLXiVUZoj';
 try{$conn = new PDO($dsn, $username, $password);}
 catch(PDOException $pe){echo $pe->getMessage();}
-if($conn){echo "Connexion MySQL établie !\r\n";}
+if($conn){;}
 $response=array();
 
 if(isset($_POST['success']) && !empty($_POST['success']))
@@ -22,12 +22,11 @@ if(isset($_POST['success']) && !empty($_POST['success']))
             $sql="INSERT INTO table_dht(dht_success, dht_temperature, dht_humidity)
             VALUES ('$success', '$temperature', '$humidity')";
 
-            try{$conn->query($sql);echo "Donnée(s) stockée(s)\r\n";}
-            catch(Exception $pe){echo "Donnée(s) non stockée(s)\r\n";}
+            try{$conn->query($sql);}
+            catch(Exception $pe){echo $pe->getMessage();}
         }
 
-$conn=null;echo "Connexion MySQL fermée !\r\n";
-echo "------------------- Fin programme ! ------------------- \r\n\r\n";
+$conn=null;
 ?>
 
 <!DOCTYPE html>
