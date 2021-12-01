9ac0039a8115fb3293a9849a403c9b035d6dc3d1
js <js201910271836@outlook.com>
2021-12-02 00:15:17 +0100

    02-12-21 0:15:16

--- Database/Database.php
+++ Database/Database.php
@@ -19,7 +19,7 @@ class Database
         {
             $options =
             [
-                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8; SET time_zone='Europe/Brussels'",
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8; SET time_zone='+01:00'",
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_EMULATE_PREPARES => false
             ];
