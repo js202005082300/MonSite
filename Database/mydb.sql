3a473cc999cf4055c3713b7172a2ace98dab28eb
js <js201910271836@outlook.com>
2021-12-08 21:26:59 +0100

    08-12-21 21:26:57

--- Database/mydb.sql
+++ Database/mydb.sql
@@ -1,9 +1,7 @@
-DROP DATABASE IF EXISTS `MyDB`;
-CREATE DATABASE IF NOT EXISTS `MyDB`;
-USE `MyDB`;
--- DROP DATABASE IF EXISTS `u870391923_MyDB`;
--- CREATE DATABASE IF NOT EXISTS `u870391923_MyDB`;
--- USE `u870391923_MyDB`;
+-- DROP DATABASE IF EXISTS `MyDB`;
+-- CREATE DATABASE IF NOT EXISTS `MyDB`;
+-- USE `MyDB`;
+USE `u870391923_MyDB`;
 
 DROP TABLE IF EXISTS `table_users`;
 CREATE TABLE IF NOT EXISTS `table_users`
@@ -13,7 +11,6 @@ CREATE TABLE IF NOT EXISTS `table_users`
     `user_email` VARCHAR(50) NOT NULL UNIQUE,
     `user_password` VARCHAR(255) NOT NULL,
     `user_admin` TINYINT(1) NOT NULL DEFAULT '0',
-    -- `user_online_time` BIGINT(21) NOT NULL, 
     `user_lastConnexion` DATETIME DEFAULT NOW() NOT NULL,
     `user_registerDate` DATETIME DEFAULT NOW() NOT NULL,
     PRIMARY KEY(`id_user`)
@@ -51,7 +48,7 @@ CREATE TABLE IF NOT EXISTS `table_dht`
 );
 
 DROP TABLE IF EXISTS `table_chatBox`;
-CREATE TABLE IF NOT EXISTS `table_chatBox`;
+CREATE TABLE IF NOT EXISTS `table_chatBox`
 (
     `id_message` INT(11) NOT NULL AUTO_INCREMENT,
     `message_id_user` INT(11) NOT NULL,
@@ -63,7 +60,7 @@ CREATE TABLE IF NOT EXISTS `table_chatBox`;
 INSERT INTO `table_users`(`user_name`, `user_password`, `user_email`, `user_admin`)
 VALUES
 ('SamAdmin', '$2y$10$Fdlb6svlSAJKxVEWtK.UjOUz9FZg5G17C2Ekt6MrV/AsrXFnIeM8a', 'js201910271235@outlook.com', 1),
-('Guest01', '$2y$10$2S1yQapCZG9t8gA9yoRp9.c3W5VhjaIdnuKlwJ8qOgreqksFIPbPm', 'js201910271235@outlook.com', 0),
-('Guest02', '$2y$10$i2FLupW7brb5Sx0a5TCUFO0NOKqgE0GbILUwOw6yE4aZdu5bMzHoO', 'js201910271235@outlook.com', 0);
+('Guest01', '$2y$10$2S1yQapCZG9t8gA9yoRp9.c3W5VhjaIdnuKlwJ8qOgreqksFIPbPm', 'Guest01@test.com', 0),
+('Guest02', '$2y$10$i2FLupW7brb5Sx0a5TCUFO0NOKqgE0GbILUwOw6yE4aZdu5bMzHoO', 'Guest02@test.com', 0);
 
 -- SOURCE C:/_Projets/MyWAMP/apache/htdocs/MonSiteProd/Database/mydb.sql;
