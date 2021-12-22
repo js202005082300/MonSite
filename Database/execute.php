8b16aa3dd7ad29b73000c42100ee05149aac49bd
js <js201910271836@outlook.com>
2021-12-23 00:49:26 +0100

    23-12-21 0:49:25

--- Database/execute.php
+++ Database/execute.php
@@ -4,10 +4,9 @@
 
     // init_php_session();
 
-    if(isset($_POST['valid_source']))
+    if(isset($_POST['valid_execute']))
     {
-        $req = run_sql_file("mydb.sql");
-        print_r($req);
+        run_sql_file("mydb.sql");
         echo "<p><a href='../index.php'>&laquo; Retour à l'acceuil</a></p>";
     }
 ?>
@@ -16,6 +15,6 @@
     <p>Bon on est dans le form</p>
         <fieldset form="source_form">
         <legend>Bouton d'exécution MySQL</legend>
-<input type="submit" name="valid_source" value="Requête Source" />
+<input type="submit" name="valid_execute" value="Requête execute" />
         </fieldset>
 </form>
