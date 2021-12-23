99e741b41705dfa38784736cb95b4a1e761cebc2
js <js201910271836@outlook.com>
2021-12-23 22:07:43 +0100

    23-12-21 22:07:41

--- Sensor/Download.php
+++ Sensor/Download.php
@@ -19,17 +19,17 @@ $headCSV="ID".';'.
         "DEW POINT"."\r\n";
 fwrite($myFile, $headCSV);
 foreach($req as $row):
-$rowCSV=$row['id_dht'].';'.
-        $row['dht_date'].';'.
-        $row['dht_sensorName'].';'.
-        $row['dht_location'].';'.
-        $row['dht_humidity'].';'.
-        $row['dht_tempCelsus'].';'.
-        $row['dht_tempFahrenheit'].';'.
-        $row['dht_heatIndexCelsus'].';'.
-        $row['dht_heatIndexFahrenheit'].';'.
-        $row['dht_dewPoint']."\r\n";
-fwrite($myFile, $rowCSV);
+        $rowCSV=$row['id_dht'].';'.
+                $row['dht_date'].';'.
+                $row['dht_sensorName'].';'.
+                $row['dht_location'].';'.
+                $row['dht_humidity'].';'.
+                $row['dht_tempCelsus'].';'.
+                $row['dht_tempFahrenheit'].';'.
+                $row['dht_heatIndexCelsus'].';'.
+                $row['dht_heatIndexFahrenheit'].';'.
+                $row['dht_dewPoint']."\r\n";
+        fwrite($myFile, $rowCSV);
 endforeach;
 
 if(!fclose($myFile))
