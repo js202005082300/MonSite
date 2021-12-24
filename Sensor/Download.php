3c99a8c99f639afccd0a2e5290ae3cc6c42fd2be
js <js201910271836@outlook.com>
2021-12-24 01:38:46 +0100

    24-12-21 1:38:45

--- Sensor/Download.php
+++ Sensor/Download.php
@@ -46,23 +46,3 @@ if(file_exists($fic)){
 } else {
         echo "Le fichier ".$fic." n'existe pas !";
 }
-
-// if(isset($_GET['path']))
-// {
-//         $url = $_GET['path'];
-//         clearstatcache();
-//         if(file_exists($url)) {
-//                 header('Content-Description: File Transfer');
-//                 header('Content-Type: application/octet-stream');
-//                 header('Content-Disposition: attachment; filename="'.basename($url).'"');
-//                 header('Content-Length: ' . filesize($url));
-//                 header('Pragma: public');
-//                 flush();
-//                 readfile($url,true);
-//                 die();
-//         }else{
-//                 echo "File path does not exist.";
-//         }
-// }
-
-// echo "File path is not defined."
