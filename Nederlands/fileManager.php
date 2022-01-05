1e3862f85370e6abd1ba5c7febb442f08e71c46b
js <js201910271836@outlook.com>
2022-01-05 23:54:30 +0100

    05-01-22 23:54:28

--- Nederlands/fileManager.php
+++ Nederlands/fileManager.php
@@ -1,32 +1,29 @@
 <?php
-define('ROWS', 5);
-$data = csv_to_array_03('docs/woorden.csv');
-array_to_csv($data,'docs/woorden2.csv');
-// $_POST = array();
-// array_display($data);
-$i=0;
-$j = 0;
-// shuffle($data);
 
+require 'util.php';
+define('ROWS', 10);
+$data = csv_to_array_03('docs/woorden2.csv');
 ?>
 
 <form class="oefening_form" name="oefening_form" action="" method="post">
 <fieldset form="oefening_form">
-    <legend>Oefening en woordenschat</legend>
+    <legend>Oefening en woordenschat <?php echo count_test_to_1($data).'|'.count($data) ?></legend>
 
 <?php
 echo '<div>';
-    for(; $i < ROWS; $i++)
+
+    for($i=0; $i < ROWS; $i++)
     {
-        echo $data[$i]['vertaling'].'<br>';
-        echo '<input type="text" class='.$i.' name='.$i.' size="25">';
+        echo '<br><strong>'.$data[$i]['vertaling'].'</strong><br>';
         if(isset($_POST['valid_oefening']) && !empty($_POST['valid_oefening']))
+        {
             if(isset($_POST[$i]) && !empty($_POST[$i]))
             {
+                echo '<input type="text" class='.$i.' name='.$i.' placeholder="'.$_POST[$i].'" size="25">';
                 if($_POST[$i] == $data[$i]['woord'])
                 {
-                    $data[$i]['test'] = 1;
-                    echo '<strong>Prima !</strong></br>
+                    $data[$i]['test'] = "1"."\n";
+                    echo '<em>Prima !</em></br>
                         '.$data[$i]['woord'].' ('.$data[$i]['woorden'].') = '.$data[$i]['vertaling'];
                     if($data[$i]['bijvoorbeld'])
                         echo '<br>bvb : '.$data[$i]['bijvoorbeld'];
@@ -34,169 +31,48 @@ echo '<div>';
                         echo '<br>'.$data[$i]['toelichting'];
                 }
                 else
-                    echo '<strong>False : </strong>'.$data[$i]['woord'];
+                {
+                    echo '<em>False : </em>'.$data[$i]['woord'];
+                }
                 
             }
             else
             {
-                echo '<strong>Niets !</strong>';
+                echo '<em>Niets : </em>'.$data[$i]['woord'];
             }
-
             echo '<br>';
+        } else {
+            echo '<input type="text" class='.$i.' name='.$i.' size="25">';
+        }
     }
-echo '</div>';
+
+    if(isset($_POST['valid_oefening']) && !empty($_POST['valid_oefening']))
+    {
+        $data = sort_array_by_test($data);
+        array_to_csv($data,'docs/woorden2.csv');
+        // header('fileManager.php'); 
+    }
+
+    if(isset($_POST['delete_oefening']) && !empty($_POST['delete_oefening']))
+    {
+        $_POST = array();
+        header(); 
+    }
+
+    if(isset($_POST['reset_oefening']) && !empty($_POST['reset_oefening']))
+    {
+        $data = csv_to_array_03('docs/woorden.csv');
+        array_to_csv($data,'docs/woorden2.csv');
+    }
+
+    $_POST = array();
+
+echo '</div></br></br>';
 ?>
 
     <input type="submit" name="valid_oefening" class="valid_oefening" value="valider">
+    <input type="submit" name="next_oefening" class="next_oefening" value="suivant">
+    <input type="submit" name="reset_oefening" class="reset_oefening" value="réinitialiser">
+    <input type="reset" value="effacer">
 </fieldset>
 </form>
-
-<?php
-
-//---------------------------------------
-
-function array_to_csv(array $arr, string $path) : void
-{
-    // $myFile = fopen($path, 'w');
-  
-    foreach($arr as $el){
-        echo $el;
-        break;
-        fputcsv($myFile, $el);
-    }
-      
-    // fclose($myFile);
-}
-
-function csv_to_array_01(string $path) : array
-{
-    $csv = file_get_contents($path, FILE_USE_INCLUDE_PATH);
-    $lines = explode("\n", $csv);
-    $head = str_getcsv(array_shift($lines));
-    
-    $data = array();
-    foreach ($lines as $line)
-        $data[] = array_combine($head, str_getcsv($line));
-    
-    return $data;
-}
-
-function csv_to_array_02(string $path) : array
-{
-    $myFile = fopen($path, "r");
-    if(!$myFile)
-        exit("Ouverture du fichier impossible");//die()
-    
-    $data = [];
-    $head = explode(",", trim(fgets($myFile), "\r\n"));
-    while (($line = fgetcsv($myFile, 0, ",")) !== FALSE)
-        array_push($data, array_combine($head, $line));
-    
-    if(!fclose($myFile))
-        exit("Fermeture du fichier echouee");
-    else
-        return $data;
-}
-
-function csv_to_array_03(string $path) : array
-{
-    $myFile = fopen($path, "r");
-    if(!$myFile)
-        exit("Ouverture du fichier impossible");//die()
-    
-    $data = [];
-    $head = explode(",", trim(fgets($myFile), "\r\n"));
-    while(!feof($myFile))
-        array_push($data, array_combine($head, explode(',', fgets($myFile))));
-    
-    if(!fclose($myFile))
-        exit("Fermeture du fichier echouee");
-    else
-        return $data;
-}
-
-function csv_to_object(string $path) : object
-{
-    $myFile = fopen($path, "r");
-    if(!$myFile)
-        exit("Ouverture du fichier impossible");//die()
-
-    $head = explode(",", trim(fgets($myFile), "\r\n"));
-
-    $data = new ArrayObject();
-    while (($line = fgets($myFile)) !== FALSE)
-        $data->append(array_combine($head, str_getcsv($line)));
-    
-    if(!fclose($myFile))
-        exit("Fermeture du fichier echouee");
-    else
-        return $data;
-}
-
-function count_commas_per_line(string $path) : void
-{
-    $myFile = fopen($path, "r");
-    if(!$myFile)
-        exit("Ouverture du fichier impossible");//die()
-    
-    $line = 1;
-    while(!feof($myFile))
-    {
-        $str = fgets($myFile);
-        $coma = substr_count($str, ',');
-        if($coma != 4)
-            echo 'Numéro de ligne : '.$line.' | Nombre de virgule : '.$coma.'<br>';
-    
-        $line++;
-    }
-    
-    if(!fclose($myFile))
-        exit("Fermeture du fichier echouee");
-}
-
-function array_display($a) : void
-{
-    // echo $data[0][4];
-    echo '<pre>';
-    print_r($a);
-    echo '</pre>';
-}
-
-function test_benchmark() : void
-{
-    $debut = microtime(true); 
-    $data = csv_to_array_01("docs/woorden.csv");
-    $fin = microtime(true); 
-    $temps_01 = ($fin - $debut);
-    echo "csv_to_array_01 : ".$temps_01." sec<br>";
-    
-    $debut = microtime(true); 
-    $data = csv_to_array_02("docs/woorden.csv");
-    $fin = microtime(true); 
-    $temps_02 = ($fin - $debut);
-    echo "csv_to_array_02 : ".$temps_02." sec<br>";
-    
-    $debut = microtime(true); 
-    $data = csv_to_array_03("docs/woorden.csv");
-    $fin = microtime(true); 
-    $temps_03 = ($fin - $debut);
-    echo "csv_to_array_03 : ".$temps_03." sec<br>";
-    
-    $debut = microtime(true); 
-    $data = csv_to_object("docs/woorden.csv");
-    $fin = microtime(true); 
-    $temps_04 = ($fin - $debut);
-    echo "csv_to_object : ".$temps_04." sec<br>";
-    
-    if($temps_01 < $temps_02 && $temps_01 < $temps_03 && $temps_01 < $temps_04)
-        echo 'Le vainqueur est $temps_01<br>';
-    
-    if($temps_02 < $temps_01 && $temps_02 < $temps_03 && $temps_02 < $temps_04)
-        echo 'Le vainqueur est $temps_02<br>';
-    
-    if($temps_03 < $temps_01 && $temps_03 < $temps_02 && $temps_03 < $temps_04)
-        echo 'Le vainqueur est $temps_03<br>';
-    
-    if($temps_04 < $temps_01 && $temps_04 < $temps_02 && $temps_04 < $temps_03)
-        echo 'Le vainqueur est $temps_04<br>';
-}
---
+++ Nederlands/util.php
@@ -0,0 +1,194 @@
+<?php
+
+function sort_array_by_test(array $arr) : array
+{
+    $a1 = $a2 = [];
+    foreach($arr as $el)
+    {
+        if($el['test'] == 0 || $el['test'] == "test")
+            array_push($a1, $el);
+        if($el['test'] == 1)
+            array_push($a2, $el);
+    }
+
+    foreach($a2 as $el)
+        array_push($a1, $el);
+
+    return $a1;
+}
+
+function count_test_to_1(array $arr) : int
+{
+    $count = 0;
+
+    foreach($arr as $el)
+        if($el['test'] == 1)
+            $count++;
+    
+    return $count;
+}
+
+function array_to_csv(array $arr, string $path) : void
+{    
+    $myFile = fopen($path, 'w');
+	if(!$myFile)
+		exit("Ouverture du fichier impossible");
+
+    $head = array_keys($arr[0]);
+    $str = "";
+    foreach($head as $el)
+        $str .= $el.',';
+
+    $str = substr($str, 0, -1)."\n";
+    fwrite($myFile, $str);
+
+    foreach($arr as $fields){
+        $line = "";
+        foreach($fields as $el)
+        {
+            if(substr($el, -1) != "\n" && end($fields) != $el)
+                $line .= $el.',';
+            else
+                $line .= $el;
+        }
+
+        fwrite($myFile, $line);
+    }
+      
+    if(!fclose($myFile))
+        exit("Fermeture du fichier echouee");
+}
+
+function csv_to_array_01(string $path) : array
+{
+    $csv = file_get_contents($path, FILE_USE_INCLUDE_PATH);
+    $lines = explode("\n", $csv);
+    $head = str_getcsv(array_shift($lines));
+    
+    $data = array();
+    foreach ($lines as $line)
+        $data[] = array_combine($head, str_getcsv($line));
+    
+    return $data;
+}
+
+function csv_to_array_02(string $path) : array
+{
+    $myFile = fopen($path, "r");
+    if(!$myFile)
+        exit("Ouverture du fichier impossible");//die()
+    
+    $data = [];
+    $head = explode(",", trim(fgets($myFile), "\r\n"));
+    while (($line = fgetcsv($myFile, 0, ",")) !== FALSE)
+        array_push($data, array_combine($head, $line));
+    
+    if(!fclose($myFile))
+        exit("Fermeture du fichier echouee");
+    else
+        return $data;
+}
+
+function csv_to_array_03(string $path) : array
+{
+    $myFile = fopen($path, "r");
+    if(!$myFile)
+        exit("Ouverture du fichier impossible");//die()
+    
+    $data = [];
+    $head = explode(",", trim(fgets($myFile), "\r\n"));
+    while(!feof($myFile))
+        array_push($data, array_combine($head, explode(',', fgets($myFile))));
+    
+    if(!fclose($myFile))
+        exit("Fermeture du fichier echouee");
+    else
+        return $data;
+}
+
+function csv_to_object(string $path) : object
+{
+    $myFile = fopen($path, "r");
+    if(!$myFile)
+        exit("Ouverture du fichier impossible");//die()
+
+    $head = explode(",", trim(fgets($myFile), "\r\n"));
+
+    $data = new ArrayObject();
+    while (($line = fgets($myFile)) !== FALSE)
+        $data->append(array_combine($head, str_getcsv($line)));
+    
+    if(!fclose($myFile))
+        exit("Fermeture du fichier echouee");
+    else
+        return $data;
+}
+
+function count_commas_per_line(string $path) : void
+{
+    $myFile = fopen($path, "r");
+    if(!$myFile)
+        exit("Ouverture du fichier impossible");//die()
+    
+    $line = 1;
+    while(!feof($myFile))
+    {
+        $str = fgets($myFile);
+        $coma = substr_count($str, ',');
+        if($coma != 5)
+            echo 'Numéro de ligne : '.$line.' | Nombre de virgule : '.$coma.'<br>';
+    
+        $line++;
+    }
+    
+    if(!fclose($myFile))
+        exit("Fermeture du fichier echouee");
+}
+
+function array_display($arr) : void
+{
+    // echo $data[0][4];
+    echo '<pre>';
+    print_r($arr);
+    echo '</pre>';
+}
+
+function test_benchmark() : void
+{
+    $debut = microtime(true); 
+    $data = csv_to_array_01("docs/woorden.csv");
+    $fin = microtime(true); 
+    $temps_01 = ($fin - $debut);
+    echo "csv_to_array_01 : ".$temps_01." sec<br>";
+    
+    $debut = microtime(true); 
+    $data = csv_to_array_02("docs/woorden.csv");
+    $fin = microtime(true); 
+    $temps_02 = ($fin - $debut);
+    echo "csv_to_array_02 : ".$temps_02." sec<br>";
+    
+    $debut = microtime(true); 
+    $data = csv_to_array_03("docs/woorden.csv");
+    $fin = microtime(true); 
+    $temps_03 = ($fin - $debut);
+    echo "csv_to_array_03 : ".$temps_03." sec<br>";
+    
+    $debut = microtime(true); 
+    $data = csv_to_object("docs/woorden.csv");
+    $fin = microtime(true); 
+    $temps_04 = ($fin - $debut);
+    echo "csv_to_object : ".$temps_04." sec<br>";
+    
+    if($temps_01 < $temps_02 && $temps_01 < $temps_03 && $temps_01 < $temps_04)
+        echo 'Le vainqueur est $temps_01<br>';
+    
+    if($temps_02 < $temps_01 && $temps_02 < $temps_03 && $temps_02 < $temps_04)
+        echo 'Le vainqueur est $temps_02<br>';
+    
+    if($temps_03 < $temps_01 && $temps_03 < $temps_02 && $temps_03 < $temps_04)
+        echo 'Le vainqueur est $temps_03<br>';
+    
+    if($temps_04 < $temps_01 && $temps_04 < $temps_02 && $temps_04 < $temps_03)
+        echo 'Le vainqueur est $temps_04<br>';
+}
+?>
